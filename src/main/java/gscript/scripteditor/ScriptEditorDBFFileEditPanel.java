7f04b4358c4596e8ed874faa776303a481468ebe
spring <spring@esc.ru>
2017-06-13 15:31:15 +0300

    dev

--- src/main/java/gscript/scripteditor/ScriptEditorDBFFileEditPanel.java
+++ src/main/java/gscript/scripteditor/ScriptEditorDBFFileEditPanel.java
@@ -3,30 +3,64 @@ package gscript.scripteditor;
 import gscript.factory.file.dbf.GroovyDBFReader;
 import gscript.ui.Dialogs;
 import gscript.ui.util.TableUtils;
+import org.fife.ui.rtextarea.SearchContext;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public final class ScriptEditorDBFFileEditPanel extends ScriptEditorAbstractEditPanel {
 
     private final JTable table;
     private final JLabel statusLabel;
 
+    private int searchFromRow;
+    private int searchFromCol;
+    private SearchContext searchContext;
+
     public ScriptEditorDBFFileEditPanel(File file) {
         this.file = file;
 
         setLayout(new BorderLayout());
-        table = new JTable();
+        table = new JTable() {
+            @Override
+            public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+                final JLabel label = (JLabel) super.prepareRenderer(renderer, row, column);
+                if (isSearchPanelVisible())
+                    highlightFound(label, row, column);
+                return label;
+            }
+        };
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         table.setRowSelectionAllowed(false);
+        table.setCellSelectionEnabled(true);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setFillsViewportHeight(true);
+        table.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (isSearchPanelVisible()) {
+                    if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
+                        cancelSearch();
+
+                    if (e.getKeyCode() == KeyEvent.VK_F3 && !e.isShiftDown())
+                        findNext(true);
+
+                    if (e.getKeyCode() == KeyEvent.VK_F3 && e.isShiftDown())
+                        findNext(false);
+                }
+            }
+        });
 
         add(new JScrollPane(table), BorderLayout.CENTER);
 
@@ -36,8 +70,15 @@ public final class ScriptEditorDBFFileEditPanel extends ScriptEditorAbstractEdit
         loadFile("windows-1251");
     }
 
+    private void highlightFound(JLabel cellRenderer, int row, int column) {
+
+    }
+
     @Override
     protected void showSearchPanel(ScriptEditorSearchPanel searchPanel) {
+        searchFromRow = table.getSelectedRow();
+        searchFromCol = table.getSelectedColumn();
+
         removeAll();
         add(searchPanel, BorderLayout.NORTH);
         add(new JScrollPane(table), BorderLayout.CENTER);
@@ -47,12 +88,29 @@ public final class ScriptEditorDBFFileEditPanel extends ScriptEditorAbstractEdit
 
     @Override
     protected void findFirst(String searchText, boolean matchCase, boolean regularExpression, boolean searchForward, boolean wholeWord) {
-        // todo
+        if (table.getRowCount() > 0 && searchFromRow != -1 && searchFromCol != -1)
+            table.changeSelection(searchFromRow, searchFromCol, false, false);
+
+        searchContext = new SearchContext();
+        if (searchText.length() == 0)
+            return;
+
+        searchContext.setSearchFor(searchText);
+        searchContext.setMatchCase(matchCase);
+        searchContext.setRegularExpression(regularExpression);
+        searchContext.setSearchForward(searchForward);
+        searchContext.setWholeWord(wholeWord);
+        searchContext.setMarkAll(true);
+
+        TableSearchEngine.find(table, searchContext);
     }
 
     @Override
     protected void findNext(boolean searchForward) {
-
+        if (searchContext != null) {
+            searchContext.setSearchForward(searchForward);
+            TableSearchEngine.find(table, searchContext);
+        }
     }
 
     @Override
@@ -117,4 +175,122 @@ public final class ScriptEditorDBFFileEditPanel extends ScriptEditorAbstractEdit
         }
     }
 
+    public static final class TableSearchEngine {
+
+        public static void find(JTable table, SearchContext searchContext) {
+            if (searchContext.getSearchFor() == null || "".equals(searchContext.getSearchFor().trim()))
+                return;
+
+            final CellIterator iterator = new CellIterator(table, searchContext.getSearchForward());
+            while (iterator.hasNext()) {
+                final Cell cell = iterator.next();
+                Object valueAt = table.getValueAt(cell.row, cell.col);
+                if (matchCell(valueAt, searchContext)) {
+                    table.changeSelection(cell.row, cell.col, true, false);
+                    break;
+                }
+            }
+        }
+
+        private static class Cell {
+            int row, col;
+
+            public Cell(int row, int col) {
+                this.row = row;
+                this.col = col;
+            }
+        }
+
+        private static class CellIterator implements Iterator<Cell> {
+
+            private final JTable table;
+            private final boolean searchForward;
+            private final Cell currentCell;
+
+            public CellIterator(JTable table, boolean searchForward) {
+                this.table = table;
+                this.searchForward = searchForward;
+                this.currentCell = new Cell(
+                        table.getSelectedRow() != -1 ? table.getSelectedRow() : 0,
+                        table.getSelectedColumn() != -1 ? table.getSelectedColumn() : 0);
+            }
+
+            @Override
+            public boolean hasNext() {
+                if (searchForward) {
+                    if (currentCell.col < table.getColumnCount()) {
+                        currentCell.col++;
+                        return true;
+                    } else if (currentCell.row < table.getRowCount()) {
+                        currentCell.row++;
+                        currentCell.col = 0;
+                        return true;
+                    }
+                } else {
+                    if (currentCell.col > 0) {
+                        currentCell.col--;
+                        return true;
+                    } else if (currentCell.row > 0) {
+                        currentCell.row--;
+                        currentCell.col = table.getColumnCount();
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            @Override
+            public Cell next() {
+                return currentCell;
+            }
+
+            @Override
+            public void remove() {
+
+            }
+        }
+
+        private static boolean searchWords(String searchText, String initialString, boolean matchCase) {
+            final String[] words = searchText.split(" ");
+            if (words.length == 0)
+                return false;
+            if (words.length == 1)
+                return matchCase ? initialString.contains(words[0]) : initialString.toLowerCase().contains(words[0].toLowerCase());
+
+            for (String word : words) {
+                if (matchCase) {
+                    if (!initialString.contains(word))
+                        return false;
+                } else {
+                    if (!initialString.toLowerCase().contains(word.toLowerCase()))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static boolean matchCell(Object cellValue, SearchContext searchContext) {
+            if (cellValue == null)
+                return false;
+
+            final String searchText = searchContext.getSearchFor();
+            final boolean matchCase = searchContext.getMatchCase();
+            final boolean regularExpression = searchContext.isRegularExpression();
+            final boolean wholeWord = searchContext.getWholeWord();
+
+            if (regularExpression) {
+                return Pattern.compile(searchText).matcher(cellValue.toString()).find();
+            } else if (wholeWord) {
+                if (matchCase)
+                    return cellValue.toString().contains(searchText);
+                else
+                    return cellValue.toString().toLowerCase().contains(searchText.toLowerCase());
+            } else {
+                return searchWords(searchText, cellValue.toString(), matchCase);
+            }
+        }
+
+    }
 }
